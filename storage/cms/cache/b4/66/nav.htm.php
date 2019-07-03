4a69bef7167ae0a30b1e2c728108a7df12b5a0dd
Isaacs <isaacseliecer15@gmail.com>
2019-07-03 12:30:15 -0500

    Creacion de listado de pagos por león
    .

--- storage/cms/cache/b4/66/nav.htm.php
+++ storage/cms/cache/b4/66/nav.htm.php
@@ -1,9 +1,13 @@
 <?php 
 <<<<<<< testing
+<<<<<<< testing
 class Cms5d1cd1b93474c746611771_cd0caeabb745733301685a91580cbd4bClass extends Cms\Classes\PartialCode
 =======
 class Cms5d1be8e48b962541838799_963b756e8dd407401d486470b0649852Class extends Cms\Classes\PartialCode
 >>>>>>> Actualización- de Crear nuevo pago
+=======
+class Cms5d1cd1b93474c746611771_cd0caeabb745733301685a91580cbd4bClass extends Cms\Classes\PartialCode
+>>>>>>> Creacion de listado de pagos por león
 {
 
 }
--- storage/cms/cache/bd/f7/default.htm.php
+++ storage/cms/cache/bd/f7/default.htm.php
@@ -1,9 +1,13 @@
 <?php 
 <<<<<<< testing
+<<<<<<< testing
 class Cms5d1cd1ade54d5856371172_d14702e9e70ddf95e910e886bbe3d96fClass extends Cms\Classes\LayoutCode
 =======
 class Cms5d1be8e3e589d110991757_0965c074c4af25475a539ab9cb9e8cbeClass extends Cms\Classes\LayoutCode
 >>>>>>> Actualización- de Crear nuevo pago
+=======
+class Cms5d1cd1ade54d5856371172_d14702e9e70ddf95e910e886bbe3d96fClass extends Cms\Classes\LayoutCode
+>>>>>>> Creacion de listado de pagos por león
 {
 
 }
