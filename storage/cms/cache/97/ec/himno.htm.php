8a6e73d2daa5ae2328fa1111b35fe3d68ad381a0
Jose <j.andres4444@gmail.com>
2019-07-06 13:49:15 -0500

    arreglos-vistahimno
    Algunas modificaciones a la vista del himno de istmania

--- storage/cms/cache/97/ec/himno.htm.php
+++ storage/cms/cache/97/ec/himno.htm.php
@@ -1,5 +1,5 @@
 <?php 
-class Cms5d1cb2056b5c1917811028_7fe272e48a515e52f268761ac6bbe2d6Class extends Cms\Classes\PageCode
+class Cms5d20ec8dba96d324356376_d603c000ee09e05e20def087ff38383dClass extends Cms\Classes\PageCode
 {
 
 }
